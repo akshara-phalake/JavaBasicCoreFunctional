72bf09d3e9c9e277da432e9802885f763426ecc3
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-03-04 23:15:37 +0530

    Added: Harmonic and Prime factorisation

---
+++ src/com/examples/javabasicfunctional/HarmonicNumber.java
@@ -0,0 +1,23 @@
+package com.examples.javabasicfunctional;
+
+import java.util.Scanner;
+
+public class HarmonicNumber {
+    public static void main(String[] args) {
+        System.out.println("Enter the value of n");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        double sum = 0;
+        System.out.println("Harmonic Series is ");
+        if (n>0) {
+            for (double i = 1; i <= n; i++) {
+                sum += 1 / i;
+                System.out.print("1/"+i +"+");
+            }
+            System.out.println("\n The harmonic sum is " + sum);
+        }
+        else{
+            System.out.println("n cannot be zero");
+        }
+    }
+}
---
+++ src/com/examples/javabasicfunctional/PrimeFactorisation.java
@@ -0,0 +1,26 @@
+package com.examples.javabasicfunctional;
+
+import java.util.Scanner;
+
+public class PrimeFactorisation {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = in.nextInt();
+        System.out.println("The prime factors of " +n +" =");
+        while (n % 2 == 0) {                                // If n is even, print 2. Divide n by 2
+            System.out.print(2 + " ");
+            n = n/2;
+        }
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {        //If n is odd, i will take values from 3 till squareroot of n, i=i+2 gives odd numbers only
+            while (n % i == 0) {                            //If n is divisible by i
+                System.out.print(i + " ");                  //Print i
+                n = n/i;                                    //Divide n by i
+            }                                               //for loop continues
+        }
+        if (n > 2)                                          //Now if n is greater than 2, it will be a prime number greater than 2
+            System.out.print(n);                            //Print the value of n
+
+    }
+}
