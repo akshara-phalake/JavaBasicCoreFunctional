2243d871f6a53f053533bbfd4f2bc45b8f028e60
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-03-08 08:32:13 +0530

    Added: Functional Sum of Integers

--- src/com/examples/functional/SumOfThreeIntegers.java
+++ src/com/examples/functional/SumOfThreeIntegers.java
@@ -1,7 +1,32 @@
 package com.examples.functional;
+import java.util.Scanner;
 
 public class SumOfThreeIntegers {
+    static Scanner s = new Scanner(System.in);
+
+    static void sum0count(int n) {
+        int[] arr = new int[n];
+        int count = 0;
+        System.out.println("Enter the values");
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        count++;
+                        System.out.println(arr[i] + "+" + arr[j] + "+" + arr[k] + "=" + "0");
+                    }
+                }
+            }
+        }
+        System.out.println("Total pairs are " + count);
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println("Enter no of inputs");
+        int n = s.nextInt();
+        sum0count(n);
     }
 }
